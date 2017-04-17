7e358b671578faa36839a813cde46dd0bf3c62a6
Kangchen Bai <kbai@titaia.gps.caltech.edu>
2017-04-17 11:20:06 -0700

    Add latent factor model and time dependent latent factor model.

--- src/kbai/latenfactor10_timedependent.cpp
+++ src/kbai/latenfactor10_timedependent.cpp
@@ -1,13 +1,13 @@
 #include "commonheader.h"
 #include <cstdlib>
-#define NLAT 10 // number of latent factors
+#define NLAT 20
 
 using namespace std;
 
 float dotprod(const vector<float>& v1, const vector<float>& v2)
 {
 	float sum = 0.0;
-	for(int i = 0; i < v1.size(); i++)
+	for(int i = 0 ; i < v1.size(); i++)
 		sum += v1[i] * v2[i];
 	return sum;
 }
@@ -44,6 +44,7 @@ struct bkmodel
 	vector<vector<float>> bm; // 50 latent factors
 	vector<float> bu;
 	vector<vector<float>> bs; // 50 latent factors
+	vector<vector<vector<float>>> bst;
 	vector<float> btu;
 	vector<vector<float>> bt;
 	float mean;
@@ -57,13 +58,14 @@ struct bkmodel
 };
 
 bkmodel::bkmodel(): uuser(NUSER,0), umovie(NMOVIE,0), bm(NMOVIE,vector<float>(NLAT,0)) // 50 latent factors
- ,bu(NUSER,0), bs(NUSER,vector<float>(NLAT,0)), btu(NUSER,0), bt(NMOVIE,vector<float>(30,0))
+ ,bu(NUSER,0), bs(NUSER,vector<float>(NLAT,0)), btu(NUSER,0), bt(NMOVIE,vector<float>(30,0)), bst(NMOVIE,vector<vector<float>>(30,vector<float>(NLAT,0)))
 {
 	for(auto &i : bu)  i = 1.0*rand()/RAND_MAX;  //random initialization
 	for(auto &i : bs) for(auto &j : i)  j = 1.0*rand()/RAND_MAX/NLAT; // initialization of latent factors
 	for(auto &i : bm) for(auto &j : i)  j = 1.0*rand()/RAND_MAX/NLAT;
 	for(auto &i : btu) i = 1.0*rand()/RAND_MAX;
 	for(auto &i : bt) for(auto &j : i)  j = 1.0*rand()/RAND_MAX;
+	for(auto &i : bst) for(auto &j : i) for(auto &k : j) k = 0.0;
 	mean = rand()*1.0/RAND_MAX;
 	ifstream userfile("../data/userfile.dta");
 	ifstream moviefile("../data/moviefile.dta");
@@ -80,7 +82,7 @@ bkmodel::bkmodel(): uuser(NUSER,0), umovie(NMOVIE,0), bm(NMOVIE,vector<float>(NL
 float bkmodel::g(int iu, int im, int it)
 {
 	float gv = 0.0;
-	gv = mean + bu[iu] + dotprod(bm[im],bs[iu])  + bt[im][it]*btu[iu];
+	gv = mean + bu[iu] + dotprod(bm[im],bs[iu]) + dotprod(bst[im][it],bs[iu])  + bt[im][it]*btu[iu];
 	return gv;
 }
 
@@ -102,7 +104,9 @@ void bkmodel::update_param_sgd(feature &a)
 	for(int i = 0; i < NLAT; i++)
 	{
 		bm[im][i] -= lr*(-2.0*error*bs[iu][i] + 2.0*bm[im][i]*(0.00234879 + 0.0610418/umovie[im])) ;
-		bs[iu][i] -= lr*(-2.0*error*bm[im][i] + 2.0*bs[iu][i] *(0.0753211 + 3.11288/uuser[iu]));
+		bs[iu][i] -= lr*(-2.0*error*(bm[im][i]+bst[im][it][i]) + 2.0*bs[iu][i] *(0.0753211 + 3.11288/uuser[iu]));
+		bst[im][it][i]  -= 0.1*lr*(-2.0*error*bs[iu][i] + 2.0*bst[im][it][i]*(0.0234879 + 0.610418/umovie[im])) ;
+
 	}
 //	cout << "ddd" << endl;
 	bt[im][it] -= lr*(-2.0*error*btu[iu])+ bt[im][it] * 0.0001;
@@ -111,37 +115,6 @@ void bkmodel::update_param_sgd(feature &a)
 }
 
 
-
-/*int sgd(vector<int> & uuser, vector<int> & umovie,float &mean, vector<float> &bu, vector<vector<float>> &bm,vector<vector<float>>& bs,vector<vector<float>>& bt,vector<float>& btu, ifstream &ofile,float lambda, float alpha ,float lr)
-{
-	int iu;
-	int im;
-	int it;
-	int a;
-	int rate;
-	float error;
-	if(!(ofile >> iu  >> im  >> it >> a>> rate))
-	{
-		cout << "here is the end" << endl;
-		ofile.clear();
-		ofile.seekg(0,ios::beg);// if read fails go to beginning
-		ofile >> iu  >> im  >> it >> a>> rate;
-
-	}
-	//cout << iu <<"\t"<< im <<"\t" << rate << endl;
-	iu--;
-	im--;
-	error = -(mean + bu[iu] + bm[im]*(1+bs[iu]) + bt[im][it]*btu[iu]) + rate ;
-	bu[iu] -= lr*(-2.0*error + 2.0*bu[iu]*(lambda + alpha/uuser[iu]));
-//	cout << bu[17772] << endl;
-	bm[im] -= lr*(-2.0*error*(1+bs[iu]) + 2.0*bm[im]*(0.00234879 + 0.0610418/umovie[im])) ;
-	bs[iu] -= lr*(-2.0*error*bm[im] + 2.0*bs[iu] *(0.0753211 + 3.11288/uuser[iu]));
-	bt[im][it] -= lr*(-2.0*error*btu[iu])+ bt[im][it] * 0.0001;
-	btu[iu] -= lr*(-2.0*error*bt[im][it]) + btu[iu] * 0.001;
-	mean -= lr*(-2.0*error)/100;
-	return 0;
-}
-*/
 double compute_RMSE(bkmodel &bk, const vector<vector<int>> &testset)
 {
 	int im,iu,rate,it;
@@ -168,7 +141,7 @@ double compute_QUAL(bkmodel &bk, const vector<vector<int>> &testset)
 	int im,it,iu,rate;
 	int n = testset.size();
 	double rmse = 0.0;
-	ofstream qual("latenfactormodel10.dta");
+	ofstream qual("timedependent_latenfactormodel10.dta");
 	for(auto & p: testset)
 	{
 		iu = p[0]-1;
