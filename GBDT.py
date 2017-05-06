9042878072f974506d349d4c1f4d12f79b7f08c1
dusm3721 <dusm3721@gmail.com>
2017-05-06 15:42:58 -0700

    Add files via upload

---
+++ GBDT.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy import genfromtxt 
+from sklearn.ensemble import GradientBoostingRegressor
+
+
+model1 = GradientBoostingRegressor(n_estimators = 200, max_depth = 20, learning_rate = 0.18, subsample = 0.9)
+model2 = GradientBoostingRegressor(n_estimators = 150, max_depth = 20, learning_rate = 0.20, subsample = 1.0)
+model1.fit(x_train, y_train)
+y_predict = model1.predict(x_predict)
+
+
+
+
